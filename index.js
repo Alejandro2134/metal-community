edbf2578988ae7b339cef45173d54581be663b14
Alejandro Zapata Molina <zapatamolinaalejandro@gmail.com>
2025-04-22 20:32:48 -0500

    feat: new code

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello, World!" });
 });
 
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello, API!" });
+});
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
